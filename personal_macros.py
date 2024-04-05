b81edee358a880200bc07679abb5bdb4f5363b96
John Millsap <jmillsapengineer@gmail.com>
2024-04-05 16:39:57 -0500

    added code for gpkg ref/deref

--- personal_macros.py
+++ personal_macros.py
@@ -1,11 +1,26 @@
 def personal_openProject():
     import console
+    from qgis.core import QgsExpressionContextUtils, QgsProviderRegistry, QgsProject
     from qgis.utils import iface
     from qgis.PyQt.QtWidgets import QDockWidget
 
     pythonConsole = iface.mainWindow().findChild(QDockWidget, 'PythonConsole')
     if not pythonConsole or not pythonConsole.isVisible():
         iface.actionShowPythonDialog().trigger()
+
+    project = QgsProject.instance()
+    if QgsExpressionContextUtils.projectScope(project).variable('project_gpkg_connections'):
+        proj_conn = QgsExpressionContextUtils.projectScope(project).variable('project_gpkg_connections')
+        md = QgsProviderRegistry.instance().providerMetadata('ogr')
+        if md.connections() != {}:
+            for key in md.connections().keys():
+                if key not in proj_conn.keys():
+                    md.deleteConnection(key)
+        for key in proj_conn.keys():
+            if key not in md.connections().keys():
+                conn = md.createConnection(proj_conn[key])
+                md.saveConnection(conn, key) 
+        iface.reloadConnections()
 
 def personal_saveProject():
     pass
